50bd2729effb4f6adae114aaca9470b8e22f4381
rsha0812 <shamim.rukaiya@gmail.com>
2020-07-05 01:05:56 +0530

    Assignment Added with all files required

---
+++ EVA4-S9--master/Albumentation.py
@@ -0,0 +1,34 @@
+
+# imports
+import numpy as np
+import albumentations as alb
+import albumentations.pytorch as alb_pytorch
+from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, VerticalFlip, Resize, Rotate
+from albumentations.pytorch import ToTensor
+
+
+
+class album_compose:
+    def __init__(self):
+        self.alb_transform = Compose([
+            HorizontalFlip(p=0.5),
+            VerticalFlip(p=0.5),
+            Rotate(limit=(-90, 90)),
+            Cutout(num_holes=1, max_h_size=8, max_w_size=8),
+            Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            ),
+            ToTensor()
+        ])
+    def __call__(self, img):
+        img = np.array(img)
+        img = self.alb_transform(image=img)['image']
+        return img
+
+
+
+
+
+
+
